47c689505db3aad031d199d774951ffb44f78a84
hyyeong <hyyeong@users.noreply.github.com>
2023-01-02 17:07:49 +0900

    Initialize Project

---
+++ cookgo/src/main/java/com/hjy/cookgo/CookgoApplication.java
@@ -0,0 +1,13 @@
+package com.hjy.cookgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookgoApplication.class, args);
+	}
+
+}
---
+++ cookgo/src/test/java/com/hjy/cookgo/CookgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjy.cookgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
